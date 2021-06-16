ce34e38b337e2d9c7419f24ec05a693694e31c2b
Michael Hansen <mh1986@outlook.dk>
2021-06-16 21:58:59 +0200

    wip

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,18 +1,16 @@
 @extends('layouts.app')
 @section('title', 'Home')
 @section('content')
-    <main class="py-4">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">Home</div>
-                        <div class="card-body">
-                            You are logged in, as <strong>{{ auth()->user()->name }}</strong>!
-                        </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Home</div>
+                    <div class="card-body">
+                        You are logged in, as <strong>{{ auth()->user()->name }}</strong>!
                     </div>
                 </div>
             </div>
         </div>
-    </main>
+    </div>
 @endsection
